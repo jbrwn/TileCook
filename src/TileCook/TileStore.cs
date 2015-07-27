1a2d179150ef3992b4c7ce250309d031f6d038cc
Joel Brown <joelsbrown1@gmail.com>
2015-07-27 01:17:54 -0700

    add Copy extension and tests

---
+++ src/TileCook/TileStore.cs
@@ -0,0 +1,34 @@
+﻿using TileProj;
+
+namespace TileCook
+{
+    public static class TileStore
+    {
+        public static void Copy(this ITileStore source, IWritableTileStore target)
+        {
+            SphericalMercator sm = new SphericalMercator();
+
+            //scanline
+            for (int i = target.MinZoom; i <= target.MaxZoom; i++)
+            {
+                IEnvelope env = sm.LongLatToXY(target.Bounds);
+                ICoord LL = sm.PointToCoord(env.LL, i);
+                ICoord UR = sm.PointToCoord(env.UR, i);
+
+                for (int j = LL.X; j <= UR.X; j++)
+                {
+                    for (int k = LL.Y; k >= UR.Y; k--)
+                    {
+                        ICoord coord = new Coord(i, j, k);
+                        VectorTile tile = source.GetTile(coord);
+                        if (tile != null)
+                        {
+                            target.PutTile(coord, tile);
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+}
---
+++ test/TileCook.Test/CopyTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using TileProj;
+using TileCook;
+
+namespace TileCook.Test
+{
+    public class MockStore : ITileStore
+    {
+        public VectorTile GetTile(ICoord coord)
+        {
+            return new VectorTile(new byte[0]);
+        }
+        public int MinZoom { get; set; }
+        public int MaxZoom { get; set; }
+        public IEnvelope Bounds { get; set; }
+    }
+
+    public class MockWritableStore : IWritableTileStore
+    {
+
+        public VectorTile GetTile(ICoord coord)
+        {
+            return new VectorTile(new byte[0]);
+        }
+        public void PutTile(ICoord coord, VectorTile tile)
+        {
+            Count++;
+            return;
+        }
+        public int MinZoom { get; set; }
+        public int MaxZoom { get; set; }
+        public IEnvelope Bounds { get; set; }
+        public int Count { get; private set; }
+        
+    }
+
+    public class CopyTests
+    {
+        [Fact]
+        public void Copy()
+        {
+            MockStore source = new MockStore();
+            MockWritableStore target = new MockWritableStore()
+            {
+                Bounds = new Envelope(-180, -90, 180, 90),
+                MinZoom = 0,
+                MaxZoom = 5
+            };
+            source.Copy(target);
+            Assert.Equal(1365, target.Count);
+        }
+    }
+}
