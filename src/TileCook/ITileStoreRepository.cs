75999617cf01061078e22fe9b3ac2714e732fce4
Joel Brown <joelsbrown1@gmail.com>
2015-08-02 22:54:57 -0700

    Add TileStore repo

---
+++ src/TileCook/ITileStoreRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace TileCook
+{
+    public interface ITileStoreRepository
+    {
+        void AddOrUpdate(ITileStore tilestore);
+        void Delete(ITileStore tilestore);
+        ITileStore GetById(string Id);
+    }
+}
---
+++ src/TileCook/MemoryTileStoreRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+
+namespace TileCook
+{
+    public class MemoryTileStoreRepository : ITileStoreRepository 
+    {
+        private static ConcurrentDictionary<string, ITileStore> MemoryRepo;
+
+        public MemoryTileStoreRepository()
+        {
+            MemoryRepo = new ConcurrentDictionary<string, ITileStore>();
+        }
+
+        public void AddOrUpdate(ITileStore tilestore)
+        {
+            MemoryRepo[tilestore.Id] = tilestore;
+        }
+
+        public void Delete(ITileStore tilestore)
+        {
+            ((IDictionary<string, ITileStore>)MemoryRepo).Remove(tilestore.Id);
+        }
+
+        public ITileStore GetById(string Id)
+        {
+            ITileStore t;
+            MemoryRepo.TryGetValue(Id, out t);
+            return t;
+        }
+    }
+}
