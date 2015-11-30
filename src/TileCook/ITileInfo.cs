dc881a2d28dc03ce91dc95656fd04b2146628162
Joel Brown <joelsbrown1@gmail.com>
2015-11-30 13:15:30 -0700

    add center property to tileinfo and remove id

--- src/TileCook/ITileInfo.cs
+++ src/TileCook/ITileInfo.cs
@@ -4,12 +4,12 @@ namespace TileCook
 {
     public interface ITileInfo
     {
-		string Id { get; set; }
         string Name { get; set; }
         string Description { get; set; }
         int MinZoom { get; set; }
         int MaxZoom { get; set; }
         double[] Bounds { get; set; }
+        double[] Center {get; set;}
         IEnumerable<VectorLayer> VectorLayers { get; set; }
 	}
 }
